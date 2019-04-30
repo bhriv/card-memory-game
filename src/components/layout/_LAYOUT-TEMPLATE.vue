1f971eac73b25ed92b078d748be63642fa5cfcf5
Benjamin Richards IV <ben.bhriv@gmail.com>
2019-04-30 15:57:51 -0700

    Create Reusable Stub
    When creating a new component use this as a default then switch out variables and names

---
+++ src/components/layout/_LAYOUT-TEMPLATE.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <!-- {{ getMutationValueFromStore }} -->
+  </section>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+import store from '@/store';
+
+export default {
+  name: "templateNAME",
+  props: [""],
+  mounted () {
+    console.log('templateNAME mounted now');
+  },
+  methods: {
+    // myFunction: function(){
+    //   console.log('myFunction called: ')
+    // }
+  },
+  computed: { 
+    // retrieve current data state 
+    // getMutationValueFromStore () { // is computation is called by {{ getMutationValueFromStore }} in the template
+    //   return store.state.data_value_in_store
+    // },
+  },
+}
+</script>
+
+
